a7935c0fc26fa18a157de3ef06f26e860363e64a
Vishnu Prakash <119521719+vishnuprksh@users.noreply.github.com>
2025-01-27 22:18:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -70,10 +70,10 @@ PURPOSE:
 - Change the following transcript into a detailed study note (talking style to layman style explanation). 
 IMPORTANT:
 - Mention the source of the video (Source: {source_url})
-- Strictly do not lose data during conversion.
+- Strictly do not lose data during conversion even if it seems irrelevant.
 - If equations are mentioned provide latex code
-- If images are used in the video to explain a topic, use a name (eg: fig1, fig2...etc.) and explain the topic in the note. Strictly do not miss it.
-- For the mentioned images, provide placeholders in the note.
+- If images can be used to make better explanation, use a name (eg: fig1, fig2...etc.) and explain the topic in the note. Strictly do not miss it.
+- For the mentioned images, provide placeholders in the note(name and a brief explantion).
 - The placeholders should be properly placed during the note, do not keep it for end of the note.
 """
     if model == "GPT-4o-mini":
